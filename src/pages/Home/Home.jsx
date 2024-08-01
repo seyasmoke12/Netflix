d1af018d0f2cf118d60895dc0d793ad651331dca
seya smoke <seyasmoke49@gmail.com>
2024-08-02 00:05:29 +0300

    banner file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Banner from '../../components/Banner/Banner'
+
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 
@@ -6,6 +8,7 @@ function Home() {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   )
--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,11 @@
 import axios from "axios";
-
+import React from "react";
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
 });
+
+
+
+
 
 export default instance;
