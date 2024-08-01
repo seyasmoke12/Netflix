5afc0da1eae9cb414e51de592e1bebb054b6ed92
seya smoke <seyasmoke49@gmail.com>
2024-08-02 01:30:19 +0300

    Row and RowList files and packeges

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import Banner from '../../components/Banner/Banner'
 
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
+import RowList from '../../components/Rows/RowList/RowList'
 
 function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   )
